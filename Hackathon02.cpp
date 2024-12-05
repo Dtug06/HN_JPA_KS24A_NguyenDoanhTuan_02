61110d1cff6a0d2539ad7623b95b0e692847ad06
Nguyễn Doanh Tuấn <tuan20062012@gmail.com>
2024-12-05 11:10:44 +0700

    Hackathon02.cpp

--- Hackathon02.cpp
+++ Hackathon02.cpp
@@ -11,8 +11,7 @@ int main (){
 		printf("5 them mot phan tu vao dau mang\n");
 		printf("6 xoa 1 vi tri cu the\n");
 		printf("7 sap xep mang theo thu tu tang dan\n");
-		printf("8 yeu cau nhap 1 phan tu va tim kiem xem co ton tai trong mang hay khong(Binary search)\n");
-		printf("9 in ra toan bo so nguyen to trong mang da duoc binh phuong \n") ;
+		printf("8 yeu cau nhap 1 phan tu va tim kiem xem co ton tai trong mang hay khong(Binary search)\n");		
 		printf("10 Thoat\n"); 
 		printf("11 sap xep theo thu tu giam dan\n"); 
 	scanf("%d",&cases) ;
@@ -86,27 +85,28 @@ int main (){
 			  
 		}
 		case 7: //sap xep mang theo thu tu tang dan
-		{ int temp=0; 
-		for(int i=0;i<kichCo;i++){
-			for(int j=0;j<kichCo-i-1;j++){
-				if(array[j]>array[j+1]){
-				 temp=array[j]	;
-				 array[j]=array[j+1];
-				 array[j+1]=temp; 
-				} 
-			} 
+		{
+			for(int i=1;i<kichCo;i++){
+				int key=array[i] ;
+				int j=i-1;
+				while(j>=0&&array[j]<key) {				
+				array[j+1]=array[j] ;
+				j=j-1 ;
+			}
+			array[j+1]=key; 
 		} 
-		for(int i=0;i<kichCo;i++){
-      		printf("%d\n",array[i]) ;
-		 }
-		 break; 
+		printf("cac phan tu sau khi sp xep");
+		for(int i =0;i<kichCo;i++) {
+			printf("%d\n",array[i]); 
+		}		
 	}
+	break;
 		 case 8://yeu cau nhap 1 phan tu va tim kiem xem co ton tai trong mang hay khong
 	{int sereach=0,flag=0,viTriTimKiem; 		   	  
 		 printf("ban muon tim phan tu co gia tri la gi");
 		 scanf("%d",&sereach);
 		 for (int i=0;i<kichCo;i++){
-		 	if(array[i]=sereach) {
+		 	if(array[i]==sereach) {
 		 		flag=1;
 		 		viTriTimKiem=i; 
 				 break; 
@@ -121,7 +121,7 @@ int main (){
 		  case 11://sap xep theo thu tu giam dan
 		  {int tempA=0; 
 		  for(int i=0;i<kichCo;i++) {
-		  	for(int j=0;j=kichCo-i-1;j++) {
+		  	for(int j=0;j<kichCo-i-1;j++) {
 		  		if (array[j]<array[j+1]) {
 		  			tempA=array[j];
 					  array[j]=array[j+1];
